b9fb7c7b6f8e5bea74404a37bde4ef1ccf954db2
Sebastian Webber <sebastian@swebber.me>
2016-10-31 10:25:32 -0200

    updating gulp config for deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,37 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
+var del = require('del');
+var exec = require('child_process').execSync;
+var path = require('path');
+var gutil = require('gulp-util');
 
-gulp.task('deploy', function () {
+
+// Based on: https://github.com/jbrodriguez/hugulp/blob/master/gulp/hugo.js
+function hugo(drafts) {
+
+    var cmd = 'hugo --config=config.toml ';
+    if (drafts) {
+        cmd += ' --buildDrafts=true --verbose=true" ';
+    }
+
+    var result = exec(cmd, { encoding: 'utf-8' });
+    gutil.log('hugo: \n' + result);
+}
+
+gulp.task('hugo', function () {
+    hugo(false);
+});
+
+gulp.task('ghpages', function () {
     return gulp.src('./dist/**/*')
         .pipe(ghPages());
 });
+
+gulp.task('clean:dist', function () {
+    return del([
+        './dist/**/*'
+    ]);
+});
+
+
+gulp.task('deploy', ['clean:dist', 'hugo', 'ghpages']);
